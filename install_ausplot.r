334fd1ba6875464c6feb3bbd685fb342a60c690e
hoangnguyen177 <hoangnguyen177@gmail.com>
2019-07-25 13:38:21 +1000

    update build script

--- install_ausplot.r
+++ install_ausplot.r
@@ -1,7 +1,7 @@
 #!/usr/bin/Rscript
 
-install.packages('devtools')
-install.packages('knitr')
-install.packages('goeveg')
+install.packages('devtools', dependencies = TRUE)
+install.packages('knitr', dependencies = TRUE)
+install.packages('goeveg', dependencies = TRUE)
 library(devtools)
 install_github("ternaustralia/ausplotsR", build_vignettes = TRUE)
