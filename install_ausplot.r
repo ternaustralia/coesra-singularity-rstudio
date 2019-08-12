5245f614499c02ac747315bb68c9d2dc30715292
hoangnguyen177 <hoangnguyen177@gmail.com>
2019-08-12 14:25:40 +1000

    add dependencies

--- install_ausplot.r
+++ install_ausplot.r
@@ -1,7 +1,4 @@
 #!/usr/bin/Rscript
-
-install.packages('devtools', dependencies = TRUE)
-install.packages('knitr', dependencies = TRUE)
-install.packages('goeveg', dependencies = TRUE)
-#library(devtools)
+library(devtools)
+# not working
 install_github("ternaustralia/ausplotsR", build_vignettes = TRUE)
---
+++ install_dependencies.r
@@ -0,0 +1,14 @@
+#!/usr/bin/Rscript
+
+install.packages(c('devtools'), dependencies = TRUE)
+### for Ivan
+install.packages(c('rgdal' 'rgeos', 'animation'), dependencies = TRUE)
+install.packages(c("RProtoBuf"), dependencies = TRUE)
+install.packages(c('V8'), dependencies = TRUE)
+install.packages(c('units', 'protolite', 'jqr'), dependencies = TRUE)
+install.packages(c('velox'), dependencies = TRUE)
+install.packages("rstan", repos = "https://cran.r-project.org", dependencies = TRUE)
+### for ausplot
+install.packages(c('knitr', 'plyr', 'jsonlite', 'rmarkdown'), dependencies = TRUE)
+install.packages(c('goeveg', 'vegan'), dependencies = TRUE)
+install.packages(c('simba', 'jose', 'R.methodsS3', 'R.oo', 'R.utils', 'roxygen2'), dependencies = TRUE)
