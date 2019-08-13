ba91b8df700a63eef644245845c81f062ebe2979
hoangnguyen177 <hoangnguyen177@gmail.com>
2019-08-13 14:33:35 +1000

    install pacakges in parallel

--- install_dependencies.r
+++ install_dependencies.r
@@ -2,13 +2,9 @@
 
 install.packages(c('devtools'), dependencies = TRUE)
 ### for Ivan
-install.packages(c('rgdal', 'rgeos', 'animation'), dependencies = TRUE)
-install.packages(c("RProtoBuf"), dependencies = TRUE)
-install.packages(c('V8'), dependencies = TRUE)
-install.packages(c('units', 'protolite', 'jqr'), dependencies = TRUE)
-install.packages(c('velox'), dependencies = TRUE)
+install.packages(c('rgdal', 'rgeos', 'animation', 'RProtoBuf', 'V8'), dependencies = TRUE, Ncpus = 3)
+install.packages(c('units', 'protolite', 'jqr', 'velox'), dependencies = TRUE, Ncpus = 3)
 install.packages("rstan", repos = "https://cran.r-project.org", dependencies = TRUE)
 ### for ausplot
-install.packages(c('knitr', 'plyr', 'jsonlite', 'rmarkdown'), dependencies = TRUE)
-install.packages(c('goeveg', 'vegan'), dependencies = TRUE)
-install.packages(c('simba', 'jose', 'R.methodsS3', 'R.oo', 'R.utils', 'roxygen2'), dependencies = TRUE)
+install.packages(c('knitr', 'plyr', 'jsonlite', 'rmarkdown', 'goeveg', 'vegan'), dependencies = TRUE, Ncpus = 3)
+install.packages(c('simba', 'jose', 'R.methodsS3', 'R.oo', 'R.utils', 'roxygen2'), dependencies = TRUE, Ncpus = 3)
