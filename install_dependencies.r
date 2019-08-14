afd543f39cdf7f56d704e9ef6fd6c4f04f675ec9
hoangnguyen177 <hoangnguyen177@gmail.com>
2019-08-14 12:30:28 +1000

    move to rstudio image

--- install_dependencies.r
+++ install_dependencies.r
@@ -1,10 +1,10 @@
 #!/usr/bin/Rscript
 
-install.packages(c('devtools'), dependencies = TRUE)
+install.packages(c('devtools'), dependencies = TRUE,repos = "http://cran.us.r-project.org")
 ### for Ivan
-install.packages(c('rgdal', 'rgeos', 'animation', 'RProtoBuf', 'V8'), dependencies = TRUE, Ncpus = 3)
-install.packages(c('units', 'protolite', 'jqr', 'velox'), dependencies = TRUE, Ncpus = 3)
+install.packages(c('rgdal', 'rgeos', 'animation', 'RProtoBuf', 'V8'), dependencies = TRUE, Ncpus = 3,repos = "http://cran.us.r-project.org")
+install.packages(c('units', 'protolite', 'jqr', 'velox'), dependencies = TRUE, Ncpus = 3,repos = "http://cran.us.r-project.org")
 install.packages("rstan", repos = "https://cran.r-project.org", dependencies = TRUE)
 ### for ausplot
-install.packages(c('knitr', 'plyr', 'jsonlite', 'rmarkdown', 'goeveg', 'vegan'), dependencies = TRUE, Ncpus = 3)
-install.packages(c('simba', 'jose', 'R.methodsS3', 'R.oo', 'R.utils', 'roxygen2'), dependencies = TRUE, Ncpus = 3)
+install.packages(c('knitr', 'plyr', 'jsonlite', 'rmarkdown', 'goeveg', 'vegan'), dependencies = TRUE, Ncpus = 3,repos = "http://cran.us.r-project.org")
+install.packages(c('simba', 'jose', 'R.methodsS3', 'R.oo', 'R.utils', 'roxygen2'), dependencies = TRUE, Ncpus = 3,repos = "http://cran.us.r-project.org")
