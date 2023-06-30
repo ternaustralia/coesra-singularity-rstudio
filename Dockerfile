6a7afa9935c37315f07a7c0a54e517cf38875424
Yong Liaw <y.liaw@uq.edu.au>
2023-06-30 17:49:59 +1000

    Update R packages for RStudio

--- Dockerfile
+++ Dockerfile
@@ -18,6 +18,16 @@ RUN  apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD
   apt upgrade -y && \
   apt install -y libnss3 libasound2 qt5-default libcurl4-openssl-dev libxml2-dev libmagick++-dev git libgdal-dev libgeos-dev libprotobuf-dev libprotoc-dev protobuf-compiler libudunits2-dev libjq-dev libqpdf-dev libpoppler-cpp-dev libssl-dev libssh2-1-dev openjdk-11-jdk libv8-dev libsodium-dev > /dev/null 2>&1
 
+RUN apt install -y cmake g++ make
+
+RUN apt install -y libgmp3-dev libfribidi-dev libharfbuzz-dev fftw3-dev jags
+
+# Configure Java for R; required for rJava
+ENV JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
+ENV PATH=$PATH:$JAVA_HOME/bin
+RUN R CMD javareconf
+
+# Install R packages
 RUN chmod +x /tmp/install_dependencies.r
 
 RUN Rscript /tmp/install_dependencies.r > depedencies.log
--- install_dependencies.r
+++ install_dependencies.r
@@ -1,17 +1,16 @@
 #!/usr/bin/Rscript
 
-install.packages(c('devtools'), dependencies = TRUE,repos = "https://cran.csiro.au")
+install.packages(c('devtools'), dependencies=TRUE, repos="https://cran.csiro.au")
 ### for Ivan
-install.packages(c('animation', 'RProtoBuf', 'V8'), dependencies = TRUE, Ncpus = 3,repos = "https://cran.csiro.au")
-install.packages(c('protolite', 'jqr'), dependencies = TRUE, Ncpus = 3,repos = "https://cran.csiro.au")
-install.packages("rstan", repos = "https://cran.csiro.au", dependencies = TRUE)
-install.packages(c('goeveg', 'vegan', 'simba', 'jose'), Ncpus = 3,repos = "https://cran.csiro.au")
+install.packages(c('animation', 'RProtoBuf', 'V8'), dependencies=TRUE, Ncpus=3, repos="https://cran.csiro.au")
+install.packages(c('protolite', 'jqr'), dependencies =TRUE, Ncpus=3, repos = "https://cran.csiro.au")
+install.packages("rstan", repos="https://cran.csiro.au", dependencies=TRUE)
+install.packages(c('goeveg', 'vegan', 'simba', 'jose'), Ncpus=3, repos="https://cran.csiro.au")
 
 # pre install some pkgs which are not correctly resolved as dependencies
 #  - shiny for htmlwidgets
 #  - rcpp for pdftools
-install.packages(c('shiny', 'RCpp'),
-				 Ncpus = 3,repos = "https://cran.csiro.au")
+install.packages(c('shiny', 'RCpp'), Ncpus=3, repos="https://cran.csiro.au")
 
 install.packages(c('abind', 'askpass', 'assertthat', 'automap', 'backports',
 				'base64enc', 'BH', 'bindr', 'bindrcpp', 'bit', 'bit64', 'bitops', 'blob',
@@ -25,10 +24,10 @@ install.packages(c('abind', 'askpass', 'assertthat', 'automap', 'backports',
 				'gtable', 'haven', 'highr', 'HistData', 'Hmisc', 'hms', 'htmltools',
 				'htmlwidgets', 'httpuv', 'httr', 'igraph', 'ini', 'intervals', 'jsonlite',
 				'KernSmooth', 'knitr', 'labeling', 'Lahman', 'later', 'lattice',
-				'lazyeval', 'leaflet', 'lme4', 'lmomco', 'Lmoments', 'lubridate',
+				'lazyeval', 'leaflet', 'lme4', 'lmomco', 'Lmoments',
 				'magrittr', 'mapdata', 'maps', 'maptools', 'markdown', 'MASS', 'Matrix',
 				'MatrixModels', 'memoise', 'mgcv', 'mime', 'minqa',
-				'munsell', 'ncdf4', 'nlme', 'nloptr', 'nnet', 'openssl', 'openxlsx',
+				'munsell', 'nlme', 'nloptr', 'nnet', 'openssl', 'openxlsx',
 				'pander', 'pbkrtest', 'pdftools', 'pillar', 'pkgbuild',
 				'pkgconfig', 'pkgload', 'plogr', 'plyr', 'praise', 'prettyunits',
 				'processx', 'progress', 'promises', 'proto', 'ps', 'purrr', 'quantreg',
@@ -43,5 +42,21 @@ install.packages(c('abind', 'askpass', 'assertthat', 'automap', 'backports',
 				'tinytex', 'translations', 'units', 'usethis', 'utf8',
 				'vctrs', 'velox', 'visNetwork' , 'viridisLite', 'whisker', 'withr', 'xfun', 'xml2',
 				'xopen', 'xtable', 'xts', 'yaml', 'zeallot', 'zip', 'zoo'),
-				 Ncpus = 3,repos = "https://cran.csiro.au")
+				 Ncpus=3, repos="https://cran.csiro.au")
+
+
+install.packages(c('R2jags', 'gamm4', 'emmeans', 'dismo', 'ausplotsR', 'ozmaps', 'mgvc', 'tidyverse', 'lubridate',
+				'plantecophys', 'ncdf4', 'soundecology', 'seewave', 'bioacoustics', 'warbleR', 'hazer', 'phenocamr',
+				'monitoR' 'landsat', 'fanplot', 'usethis', 'xlsx'),
+				 Ncpus=3, repos="https://cran.csiro.au")
+
 ### 'RcppEigen' was left out, it is not working
+
+# Install packages from CRAN archieves
+devtools::install_version("slga", version="1.2.0", repos="https://cran.csiro.au")
+devtools::install_version("galah", version="1.5.2", repos="https://cran.csiro.au")
+
+# Install packages from github:
+devtools::install_github("traitecoevo/austraits", dependencies=TRUE, upgrade="always")
+devtools::install_github("ilyamaclean/microclima", dependencies=TRUE, upgrade="always")
+devtools::install_github("mrke/NicheMapR", dependencies=TRUE, upgrade="always")
